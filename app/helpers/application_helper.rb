5711efb7e73150ea052f69166069f4092879f37c
phanhuy <phanhuy.bkhn@gmail.co>
2015-01-31 11:23:22 +0700

    sign-up, log-in,log-out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def set_title(page_title ='')
+		base_title = "TravelAsk"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+		
+	end
 end
