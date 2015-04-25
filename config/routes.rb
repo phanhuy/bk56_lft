981a9cf954128f56a3f18fa7ce26834d3062fc1b
phanhuy <phanhuy.bkhn@gmail.com>
2015-04-25 10:12:58 +0700

    refs #task/29559 アンサー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions do
+	  resources :answers
+  end
 
-root 'static_pages#home'
-get 'help' => 'static_pages#help'
-get 'about' => 'static_pages#about'
-get 'signup' => 'users#new'
-get 'signin' => 'sessions#new'
-post 'signin' => 'sessions#create'
-delete 'signout' => 'sessions#destroy'
-resources :users  #routing for /users/1
+	root 'static_pages#home'
+	get 'help' => 'static_pages#help'
+	get 'about' => 'static_pages#about'
+	get 'signup' => 'users#new'
+	get 'signin' => 'sessions#new'
+	post 'signin' => 'sessions#create'
+	delete 'signout' => 'sessions#destroy'
+  devise_for :users
+  resources :answers
+  get  '/answers/:answer_id' => redirect( '/questions/:question_id/answers'	)
+  resources :users
+  
+  
 end
---
+++ db/migrate/20150423090447_add_userid_to_answer.rb
@@ -0,0 +1,5 @@
+class AddUseridToAnswer < ActiveRecord::Migration
+  def change
+  	
+  end
+end
