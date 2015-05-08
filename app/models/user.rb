8e509f7965213e2f00228caae89794bab06a3576
phanhuy <phanhuy@users.noreply.github.com>
2015-05-08 15:00:17 +0700

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
 
 	def authenticated?(remember_token)
 		#return false if remember_digest.nil?
-		BCript::Password.new(remember_digest).is_password?(remember_token)
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
 
 	def User.digest(string)
